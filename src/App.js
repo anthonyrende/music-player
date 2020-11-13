d292cfcec138ce7e52a137b2ff19406ac4350dbc
ren.meta@gmail.com <ren.meta@gmail.com>
2020-11-13 15:29:01 -0500

    initial setup of components structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import react from 'react'
+import React from 'react'
+
+// Components
+import Player from './components/Player'
+import Song from './components/Song'
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Music Player</h1>
+			<Player />
+			<Song />
 		</div>
 	)
 }
