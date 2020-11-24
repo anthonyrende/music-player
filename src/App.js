9569a2a5d9a29e8dc7ec1d16ecb007d7f75224ed
ren.meta@gmail.com <ren.meta@gmail.com>
2020-11-24 16:56:50 -0500

    adding song data state, minor styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // Styles
 import './styles/app.scss'
@@ -7,10 +7,15 @@ import './styles/app.scss'
 import Player from './components/Player'
 import Song from './components/Song'
 
+// Util
+import data from './data.js'
+
 function App() {
+	const [songs, setSongs] = useState(data())
+	const [currentSong, setCurrentSong] = useState(songs[0])
 	return (
 		<div className='App'>
-			<Song />
+			<Song currentSong={currentSong} />
 			<Player />
 		</div>
 	)
