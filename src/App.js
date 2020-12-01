61a90eb3d0055d149a28351aa241e475804e70c7
ren.meta@gmail.com <ren.meta@gmail.com>
2020-12-01 15:50:02 -0500

    song library UI components

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import './styles/app.scss'
 // Components
 import Player from './components/Player'
 import Song from './components/Song'
+import Library from './components/Library.js'
 
 // Util
 import data from './data.js'
 
 function App() {
+	// State
 	const [songs, setSongs] = useState(data())
 	const [currentSong, setCurrentSong] = useState(songs[0])
 	const [isPlaying, setIsPlaying] = useState(false)
@@ -22,6 +24,7 @@ function App() {
 				isPlaying={isPlaying}
 				currentSong={currentSong}
 			/>
+			<Library songs={songs} />
 		</div>
 	)
 }
