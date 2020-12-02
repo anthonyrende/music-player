9bd287a55c7033b804b5fe6f3bececb530070209
ren.meta@gmail.com <ren.meta@gmail.com>
2020-12-02 14:31:43 -0500

    refractoring -- lifting up state to allow side components access to song data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 
 // Styles
 import './styles/app.scss'
@@ -12,19 +12,45 @@ import Library from './components/Library.js'
 import data from './data.js'
 
 function App() {
+	// Ref
+	const audioRef = useRef(null)
 	// State
 	const [songs, setSongs] = useState(data())
 	const [currentSong, setCurrentSong] = useState(songs[0])
 	const [isPlaying, setIsPlaying] = useState(false)
+	const [songInfo, setSongInfo] = useState({
+		currentTime: 0,
+		duraction: 0,
+	})
+	// Util
+	const timeUpdateHandler = (e) => {
+		const current = e.target.currentTime
+		const duration = e.target.duration
+		setSongInfo({ ...songInfo, currentTime: current, duration })
+	}
 	return (
 		<div className='App'>
 			<Song currentSong={currentSong} />
 			<Player
+				setSongInfo={setSongInfo}
+				songInfo={songInfo}
+				audioRef={audioRef}
 				setIsPlaying={setIsPlaying}
 				isPlaying={isPlaying}
 				currentSong={currentSong}
 			/>
-			<Library songs={songs} setCurrentSong={setCurrentSong} />
+			<Library
+				audioRef={audioRef}
+				songs={songs}
+				setCurrentSong={setCurrentSong}
+				isPlaying={isPlaying}
+			/>
+			<audio
+				onTimeUpdate={timeUpdateHandler}
+				onLoadedMetadata={timeUpdateHandler}
+				ref={audioRef}
+				src={currentSong.audio}
+			></audio>
 		</div>
 	)
 }
