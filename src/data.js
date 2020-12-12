668d4e7ab28234f09884c1306d4456ab2ff58d7c
ren.meta@gmail.com <ren.meta@gmail.com>
2020-12-12 00:02:52 -0500

    added more artist data

--- src/data.js
+++ src/data.js
@@ -10,6 +10,14 @@ function chillHop() {
 			color: ['#205950', '#2ab3bf'],
 			id: uuidv4(),
 			active: true,
+			linksToArtist: {
+				spotify: 'https://chillhop.ffm.to/canaryforest.owe/spotify',
+				apple: 'https://chillhop.ffm.to/canaryforest.owe/apple',
+				bandcamp: 'https://chillhop.ffm.to/canaryforest.owe/bandcamp',
+				youtube: 'https://chillhop.ffm.to/canaryforest.owe/youtube',
+			},
+			aboutArtist:
+				'At the beginning of the year, producers Aso, Aviino, and Middle School visited The Netherlands and created a number of songs while in Rotterdam. Some in our studio, others in the trio’s haunted houseboat hotel. The end result is Canary Forest, a five song project that begins in the backyard of our Chillhop HQ and deepens down the creek, over the bridge, and into where the forest expands.',
 		},
 		{
 			name: 'Daylight',
@@ -20,6 +28,14 @@ function chillHop() {
 			color: ['#EF8EA9', '#ab417f'],
 			id: uuidv4(),
 			active: false,
+			linksToArtist: {
+				spotify: 'https://chillhop.ffm.to/dayandnight.owe/spotify',
+				apple: 'https://chillhop.ffm.to/dayandnight.owe/apple',
+				bandcamp: 'https://chillhop.ffm.to/dayandnight.owe/bandcamp',
+				youtube: 'https://chillhop.ffm.to/dayandnight.owe/youtube',
+			},
+			aboutAlbum:
+				'Time is split in half. Half day and half night. Aiguille’s cohesive EP begins with the sun: “Daylight” and “Daydream”. We hear birds, running water, the globe in motion. “Day and Night”, the third track, satisfies the symmetry and sees the setting sun. The welcoming of the moon. The project’s final two tracks, “Nightfall” and “Nightglow”, expose the evening. We hear the twilight echoes. The animals active in the trees. It’s a rhythmic experience, enjoying 24 hours in under 15 minutes. ',
 		},
 		{
 			name: 'Keep Going',
@@ -30,6 +46,14 @@ function chillHop() {
 			color: ['#CD607D', '#c94043'],
 			id: uuidv4(),
 			active: false,
+			linksToArtist: {
+				spotify: 'https://chillhop.ffm.to/reflection.owe/spotify',
+				apple: 'https://chillhop.ffm.to/reflection.owe/apple',
+				bandcamp: 'https://chillhop.ffm.to/reflection.owe/bandcamp',
+				youtube: 'https://chillhop.ffm.to/reflection.owe/youtube',
+			},
+			aboutAlbum:
+				'Born out of organic collaboration, Swørn pushes the boundaries of instrumental music. Made up of producers Hanz and Laxcity, the two reunited for the seven song EP Reflection. Like 2019’s EP Escapade, the cherry blossoms have returned and so have the bouncing electronic beats, alive and wild, hypnotic and hazey. The best addition to your summer, wherever you are. ',
 		},
 		{
 			name: 'Nightfall',
@@ -40,6 +64,14 @@ function chillHop() {
 			color: ['#EF8EA9', '#ab417f'],
 			id: uuidv4(),
 			active: false,
+			linksToArtist: {
+				spotify: 'https://chillhop.ffm.to/dayandnight.owe/spotify',
+				apple: 'https://chillhop.ffm.to/dayandnight.owe/apple',
+				bandcamp: 'https://chillhop.ffm.to/dayandnight.owe/bandcamp',
+				youtube: 'https://chillhop.ffm.to/dayandnight.owe/youtube',
+			},
+			aboutAlbum:
+				'Time is split in half. Half day and half night. Aiguille’s cohesive EP begins with the sun: “Daylight” and “Daydream”. We hear birds, running water, the globe in motion. “Day and Night”, the third track, satisfies the symmetry and sees the setting sun. The welcoming of the moon. The project’s final two tracks, “Nightfall” and “Nightglow”, expose the evening. We hear the twilight echoes. The animals active in the trees. It’s a rhythmic experience, enjoying 24 hours in under 15 minutes. ',
 		},
 		{
 			name: 'Reflection',
@@ -50,6 +82,14 @@ function chillHop() {
 			color: ['#CD607D', '#c94043'],
 			id: uuidv4(),
 			active: false,
+			linksToArtist: {
+				spotify: 'https://chillhop.ffm.to/reflection.owe/spotify',
+				apple: 'https://chillhop.ffm.to/reflection.owe/apple',
+				bandcamp: 'https://chillhop.ffm.to/reflection.owe/bandcamp',
+				youtube: 'https://chillhop.ffm.to/reflection.owe/youtube',
+			},
+			aboutAlbum:
+				'Born out of organic collaboration, Swørn pushes the boundaries of instrumental music. Made up of producers Hanz and Laxcity, the two reunited for the seven song EP Reflection. Like 2019’s EP Escapade, the cherry blossoms have returned and so have the bouncing electronic beats, alive and wild, hypnotic and hazey. The best addition to your summer, wherever you are. ',
 		},
 		{
 			name: 'Under the City Stars',
@@ -60,6 +100,14 @@ function chillHop() {
 			color: ['#205950', '#2ab3bf'],
 			id: uuidv4(),
 			active: false,
+			linksToArtist: {
+				spotify: 'https://chillhop.ffm.to/canaryforest.owe/spotify',
+				apple: 'https://chillhop.ffm.to/canaryforest.owe/apple',
+				bandcamp: 'https://chillhop.ffm.to/canaryforest.owe/bandcamp',
+				youtube: 'https://chillhop.ffm.to/canaryforest.owe/youtube',
+			},
+			aboutArtist:
+				'At the beginning of the year, producers Aso, Aviino, and Middle School visited The Netherlands and created a number of songs while in Rotterdam. Some in our studio, others in the trio’s haunted houseboat hotel. The end result is Canary Forest, a five song project that begins in the backyard of our Chillhop HQ and deepens down the creek, over the bridge, and into where the forest expands.',
 		},
 	]
 }
