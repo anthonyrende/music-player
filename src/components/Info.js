61af3378cd102e5fa9f46dd763822c4d217f874a
ren.meta@gmail.com <ren.meta@gmail.com>
2020-12-19 23:25:21 -0500

    WIP - Show More button needs to be more dynamic with better functionality

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,6 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import {
+	faTimes,
+	faLongArrowAltDown,
+	faLongArrowAltUp,
+} from '@fortawesome/free-solid-svg-icons'
 
 // Images
 import Spotify from '../assets/media/spotify.svg'
@@ -9,6 +13,11 @@ import Bandcamp from '../assets/media/bandcamp.svg'
 import Youtube from '../assets/media/youtube.svg'
 
 const Info = ({ currentSong, artistInfoStatus, setArtistInfoStatus }) => {
+	const [showMoreStatus, setShowMoreStatus] = useState(true)
+
+	// const expandTextHandler = () => {
+	// 	setShowMoreStatus(!showMoreStatus)
+	// }
 	return (
 		<>
 			<div className='info-container'>
@@ -20,28 +29,60 @@ const Info = ({ currentSong, artistInfoStatus, setArtistInfoStatus }) => {
 						icon={faTimes}
 					/>
 					<h2>Artist Info</h2>
+
 					<p>{currentSong.aboutAlbum}</p>
+					<div>
+						More
+						<FontAwesomeIcon
+							className='more-button'
+							onClick={() => setShowMoreStatus(!showMoreStatus)}
+							icon={faLongArrowAltUp}
+						/>
+					</div>
+
+					<div
+						className={`media-container ${
+							showMoreStatus ? 'media-container' : 'hide-media'
+						}`}
+					>
+						<h2>Available On</h2>
+
+						<a href={currentSong.linksToArtist.spotify} target='_blank'>
+							<img src={Spotify} />
+						</a>
+						<a href={currentSong.linksToArtist.apple} target='_blank'>
+							<img src={AppleMusic} />
+						</a>
+						<a href={currentSong.linksToArtist.bandcamp} target='_blank'>
+							<img src={Bandcamp} />
+						</a>
+						<a href={currentSong.linksToArtist.youtube} target='_blank'>
+							<img src={Youtube} />
+						</a>
+					</div>
 				</div>
-			</div>
-
-			<div className='media-container'>
-				<h2>Available On</h2>
-
-				<a href={currentSong.linksToArtist.spotify} target='_blank'>
-					<img src={Spotify} />
-				</a>
-				<a href={currentSong.linksToArtist.apple} target='_blank'>
-					<img src={AppleMusic} />
-				</a>
-				<a href={currentSong.linksToArtist.bandcamp} target='_blank'>
-					<img src={Bandcamp} />
-				</a>
-				<a href={currentSong.linksToArtist.youtube} target='_blank'>
-					<img src={Youtube} />
-				</a>
 			</div>
 		</>
 	)
 }
 
 export default Info
+
+// <a
+// 	id='show-more'
+// 	class='show-less'
+// 	// href='#show-less'
+// 	onClick={() => setShowMoreStatus(!showMoreStatus)}
+// >
+// 	Read less
+// 	<FontAwesomeIcon icon={faLongArrowAltUp} />
+// </a>
+// <a
+// 	id='show-less'
+// 	class='show-more'
+// 	// href='#show-more'
+// 	onClick={() => setShowMoreStatus(!showMoreStatus)}
+// >
+// 	Read more
+// 	<FontAwesomeIcon icon={faLongArrowAltDown} />
+// </a>
